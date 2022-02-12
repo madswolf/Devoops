caa0aa6f1ea7252d8c50f08a9a69bf7100be5373
oeje <oeje@itu.dk>
2022-02-12 16:25:38 +0100

    Added Initial_Migration and migration of followers,users, and messages

--- aspnet/Minitwit/Models/Entity/User.cs
+++ aspnet/Minitwit/Models/Entity/User.cs
@@ -1,16 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+using Microsoft.EntityFrameworkCore;
 
 namespace Minitwit.Models.Entity
 {
+    [Index(nameof(Username), IsUnique = true)]
     public class User
     {
-
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ID { get; set; }
+        public int Id { get; set; }
+        [Required]
         public string Username { get; set; }
-        public string PassWordHash { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+        [Required]
         public string Salt { get; set; }
+        [Required]
         public string Email { get; set; }
         public List<User> Follows { get; set; }
         public List<User> FollowedBy { get; set; }
