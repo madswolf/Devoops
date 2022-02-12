771e00f632339d138df83328424b10599327309c
oeje <oeje@itu.dk>
2022-02-12 16:25:38 +0100

    Added Initial_Migration and migration of followers,users, and messages

---
+++ aspnet/Minitwit/Migrations/20220212140725_Initial_Migration.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Minitwit.Migrations
+{
+    public partial class Initial_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    Salt = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AuthorId = table.Column<int>(type: "integer", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    PublishDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Flagged = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserUser",
+                columns: table => new
+                {
+                    FollowedById = table.Column<int>(type: "integer", nullable: false),
+                    FollowsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserUser", x => new { x.FollowedById, x.FollowsId });
+                    table.ForeignKey(
+                        name: "FK_UserUser_Users_FollowedById",
+                        column: x => x.FollowedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserUser_Users_FollowsId",
+                        column: x => x.FollowsId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_AuthorId",
+                table: "Posts",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserUser_FollowsId",
+                table: "UserUser",
+                column: "FollowsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "UserUser");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- scripts/migrateusers.py
+++ scripts/migrateusers.py
@@ -1,7 +1,10 @@
 import sqlite3
 from  urllib import request, parse
+import requests
+import ssl
+import json
 
-API_ENDPOINT = "https://localhost:5000/migrateAcc"
+API_ENDPOINT = "https://localhost:7077/Users/Create"
 DATABASE = "../pyshite/minitwit.db"
 conn = sqlite3.connect(DATABASE)
 cur = conn.cursor()
@@ -20,9 +23,13 @@ def post_user(record):
     user_name = record[1]
     email = record[2]
     salt, hash = parse_hash(record[3])
-    data = parse.urlencode({"username": user_name, "email": email, "salt": salt, "hash": hash}).encode()
-    request.urlopen(API_ENDPOINT, data=data)
-
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    data = {"Username": user_name, "Email": email, "Salt": salt, "PasswordHash": hash}
+    print(data)
+    response = requests.post(API_ENDPOINT, data=data,  verify=False)
+    print(response)
 
 def run():
     users = get_all_users()
