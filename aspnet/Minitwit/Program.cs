c21256308c965d76c96080dc5d3817056265909d
Osman Abdinasir Hassan <osha@itu.dk>
2022-02-12 17:08:29 +0100

    added base pages

--- aspnet/Minitwit/Program.cs
+++ aspnet/Minitwit/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<MinitwitContext>(options =>
 {
     options.UseInMemoryDatabase("Test");
@@ -43,5 +44,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
