274bf38f15c59ff131435eae1d6df69c2cce5276
npal <npal@itu.dk>
2022-02-15 14:12:21 +0100

    Added splash pages for Minitwit

---
+++ aspnet/Minitwit/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Minitwit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Minitwit");
+        }
+    }
+}
---
+++ aspnet/Minitwit/Controllers/MinitwitController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Minitwit.Controllers
+{
+    public class MinitwitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
