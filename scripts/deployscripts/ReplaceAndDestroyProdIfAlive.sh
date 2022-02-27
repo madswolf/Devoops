1c4a75c496a58b4c33f56c40be1dc457a0a5786d
Madswolf <mads@madswolf.dk>
2022-02-27 15:59:56 +0100

    CD maybe done?

--- scripts/deployscripts/ReplaceAndDestroyProdIfAlive.sh
+++ scripts/deployscripts/ReplaceAndDestroyProdIfAlive.sh
@@ -1,9 +1,11 @@
 if python3 MinitwitAlive.py $1 $2; then
-        if python3 reassign_floating_ip.py $1 $2; then
+        echo staging droplet $1 alive 
+        if python3 reassign_floating_ip.py $1 $2; then 
+            echo reassign_floating_ip successful
             echo Destroying production droplet $3
             vagrant destroy -f $3
             if python3 rename_droplet.py $1 $3 $2; then
-                echo rename of droplet $1 successful
+                echo rename of droplet $1 to $3 successful
             else 
                 echo rename of droplet $1 failed
             fi
