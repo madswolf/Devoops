5730e1a7a915a199dfcb10b1d50dda8cfaa9e555
oeje <oeje@itu.dk>
2022-02-27 19:38:34 +0100

    changed to use DROPLET_NAME environment variable in vagrant

--- scripts/deployscripts/ReplaceAndDestroyProdIfAlive.sh
+++ scripts/deployscripts/ReplaceAndDestroyProdIfAlive.sh
@@ -3,7 +3,9 @@ if python3 MinitwitAlive.py $1 $2; then
         if python3 reassign_floating_ip.py $1 $2; then 
             echo reassign_floating_ip successful
             echo Destroying production droplet $3
-            vagrant destroy -f $3
+            cd ../../aspnet/
+            export DROPLET_NAME=$3
+            vagrant destroy -f
             if python3 rename_droplet.py $1 $3 $2; then
                 echo rename of droplet $1 to $3 successful
             else 
@@ -12,10 +14,12 @@ if python3 MinitwitAlive.py $1 $2; then
         else 
             echo staging droplet $1 not found
             cd ../../aspnet/
-            vagrant destroy -f $3
+            export DROPLET_NAME=$1
+            vagrant destroy -f
         fi
 else
     echo staging droplet $1 not alive
     cd ../../aspnet/
-    vagrant destroy -f $3
+    export DROPLET_NAME=$1
+    vagrant destroy -f
 fi
