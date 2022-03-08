e856910b24383a0010a138c2b4b51590ebab2234
Osman Abdinasir Hassan <osha@itu.dk>
2022-03-08 13:55:08 +0100

    added user repo

---
+++ aspnet/Minitwit/DatabaseUtil/IUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Minitwit.Models.Entity;
+
+namespace Minitwit.DatabaseUtil
+{
+    public interface IUserRepository
+    {
+        public Task<User?> GetUserById(int id);
+
+        public Task<User?> GetUserByUsername(string username);
+
+        public List<User> GetUsers();
+
+        public void InsertUser(User user);
+
+        public Task<List<int>?> GetUserFollows(int id);
+
+        public Task<List<int>?> GetUserFollowedBy(int id);
+
+        public Task<Follow?> GetFollow(int followerId, int followeeId);
+
+        public Task Follow(int followerId, int followeeId);
+
+        public Task Unfollow(Follow follow);
+    }
+}
