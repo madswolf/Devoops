1abf186c57dda5ee374afde5c7f941f15d3b3f6a
oeje <Mads_1997@live.dk>
2022-03-16 14:45:08 +0100

    More issues resolved

--- aspnet/Minitwit/Repositories/IMessageRepository.cs
+++ aspnet/Minitwit/Repositories/IMessageRepository.cs
@@ -5,13 +5,12 @@ namespace Minitwit.Repositories
 {
     public interface IMessageRepository
     {
-        public Task<Message?> GetMessage(int id);
         public Task<List<Message>> GetPrivateTimeline(int id, List<int> follows, int limit = 30);
         public Task<List<Message>> GetMessagesByAuthorId(int id, int limit = 30);
         public Task<List<FilteredMessageDTO>> GetFilteredMessagesByAuthorId(int id, int limit = 100);
         public Task<List<Message>> GetMessages(int limit = 30);
         public Task<List<FilteredMessageDTO>> GetFilteredMessages(int limit = 100);
         public Task InsertMessage(Message message);
-        public Task FlagMessage(int messageId, bool flagged);
+        public Task<bool> FlagMessage(int messageId, bool flagged);
     }
 }
