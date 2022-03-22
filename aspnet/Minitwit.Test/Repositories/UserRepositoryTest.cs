20c55cee576484962f60ec38c70fef9241151b97
Osman Abdinasir Hassan <osha@itu.dk>
2022-03-22 13:38:13 +0100

    Base setup done

---
+++ aspnet/Minitwit.Test/Repositories/UserRepositoryTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Minitwit.Models.Context;
+using Minitwit.Models.Entity;
+using Minitwit.Repositories;
+using Minitwit.Test.Context;
+using System;
+using Xunit;
+
+namespace Minitwit.Test.Repositories
+{
+    public class UserRepositoryTest : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        private readonly MinitwitContext _context;
+        private readonly UserRepository _repository;
+
+        public UserRepositoryTest()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+            var builder = new DbContextOptionsBuilder<MinitwitContext>().UseSqlite(_connection);
+            _context = new MinitwitTestContext(builder.Options);
+            _context.Database.EnsureCreated();
+            _repository = new UserRepository(_context);
+        }
+
+        [Fact]
+        public void DummyTestConstraints()
+        {
+            _repository.InsertUser(new User { Id = 5, UserName = "user1", Email = "user5@test.com" });
+            Assert.Equal(true, true);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            _connection.Dispose();
+        }
+    }
+}
