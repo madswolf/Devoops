8b398649a9627f7864e2cdeffd22ea98e493fe9a
oeje <oeje@itu.dk>
2022-05-26 15:48:36 +0200

    Message Limit

--- aspnet/Minitwit/Models/DTO/MessageCreationDTO.cs
+++ aspnet/Minitwit/Models/DTO/MessageCreationDTO.cs
@@ -5,6 +5,7 @@ namespace Minitwit.Models.DTO
     public class MessageCreationDTO
     {
         [Required]
+        [StringLength(255, ErrorMessage = "Message should not exceed 255 characters.")]
         public string Text { get; set; }
     }
 }
